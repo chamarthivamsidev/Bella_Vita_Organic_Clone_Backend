9a8598d2b3c06af6abdaea84ebabad86e3b4a715
vamsichamarthi1147 <86275315+chamarthivamsidev@users.noreply.github.com>
2022-01-20 20:10:23 +0530

    updated

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const Products = require("../models/products");
 
 router.get("/", async (req, res) => {
-  try {
-    const products = await Products.find().lean().exec();
+  // try {
+  //   const products = await Products.find().lean().exec();
 
-    return res.render("products.ejs", { products: products });
-  } catch (err) {
-    return res.status(500).send(err.message);
-  }
+  //   return res.render("products.ejs", { products: products });
+  // } catch (err) {
+  //   return res.status(500).send(err.message);
+  // }
+  res.render("landingPage.ejs", {});
 });
 router.get("/api", async (req, res) => {
   let products = await Products.find().lean().exec();
