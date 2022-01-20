178eb338a28bae241706ef2242b770c7efb4a59f
vamsichamarthi1147 <86275315+chamarthivamsidev@users.noreply.github.com>
2022-01-20 22:41:16 +0530

    db.js changed

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 module.exports = () => {
   return mongoose.connect(
     process.env.MONGODB_URI ||
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zkdfr.mongodb.net/bellavitaData?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zkdfr.mongodb.net/bellavitaData?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
   );
 };
