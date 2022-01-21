527478239c98ce68ea9de9834b1771d78cb1f4d3
vaibhav darvekar <vabdarwekar00@gmail.com>
2022-01-21 20:38:30 +0530

    update

--- src/controllers/addtocart_controller.js
+++ src/controllers/addtocart_controller.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const Addtocart = require("../models/addtocart");
 
 router.post("/addtocart", async (req, res) => {
-    try{
-const addtocart = await Addtocart.create({
-    user_id: req.body.user_id,
-    products_id: req.body.products_id,
-})
-return res.status(200).send(addtocart)
-    }catch(err){
-        return res.status(500).send(err.message)
-    }
+  try {
+    const addtocart = await Addtocart.create({
+      user_id: req.body.user_id,
+      products_id: req.body.products_id,
+    });
+    return res.status(200).send(addtocart);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
 });
+
+module.exports = router;
