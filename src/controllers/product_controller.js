c9f94d45337eb85867e31bdf0383f159c920af18
vamsichamarthi1147 <86275315+chamarthivamsidev@users.noreply.github.com>
2022-01-21 09:58:33 +0530

    changed

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/products");
-const User = require("../controllers/user.controller");
+const User = require("../models/user.model");
 
 router.get("/", async (req, res) => {
   try {
