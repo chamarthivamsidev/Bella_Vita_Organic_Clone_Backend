4e8753777b7e8064c28407bcf01e9e082a2ec153
vamsichamarthi1147 <86275315+chamarthivamsidev@users.noreply.github.com>
2022-01-26 00:02:44 +0530

    added

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,46 +1,46 @@
-require("dotenv").config();
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth2").Strategy;
-const { v4: uuidv4 } = require("uuid");
+// require("dotenv").config();
+// const passport = require("passport");
+// const GoogleStrategy = require("passport-google-oauth2").Strategy;
+// const { v4: uuidv4 } = require("uuid");
 
-const User = require("../models/user.model");
+// const User = require("../models/user.model");
 
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-const newToken = (user) => {
-  return jwt.sign({ user: user }, process.env.JWT_SECRET_KEY);
-};
+// const newToken = (user) => {
+//   return jwt.sign({ user: user }, process.env.JWT_SECRET_KEY);
+// };
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:
-        "https://bellavitaorganic-cloned.herokuapp.com/auth/google/callback",
-      userProfileURL: "https://**www**.googleapis.com/oauth2/v3/userinfo",
-      passReqToCallback: true,
-    },
-    async function (request, accessToken, refreshToken, profile, done) {
-      let user = await User.findOne({ email: profile?._json?.email })
-        .lean()
-        .exec();
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL:
+//         "https://bellavitaorganic-cloned.herokuapp.com/auth/google/callback",
+//       userProfileURL: "https://**www**.googleapis.com/oauth2/v3/userinfo",
+//       passReqToCallback: true,
+//     },
+//     async function (request, accessToken, refreshToken, profile, done) {
+//       let user = await User.findOne({ email: profile?._json?.email })
+//         .lean()
+//         .exec();
 
-      if (!user) {
-        user = await User.create({
-          email: profile?._json?.email,
-          password: uuidv4(),
-          first_name: profile?._json?.name,
-          last_name: profile?._json?.name,
-          profilePic: profile?._json?.picture,
-        });
-      }
+//       if (!user) {
+//         user = await User.create({
+//           email: profile?._json?.email,
+//           password: uuidv4(),
+//           first_name: profile?._json?.name,
+//           last_name: profile?._json?.name,
+//           profilePic: profile?._json?.picture,
+//         });
+//       }
 
-      const token = newToken(user);
+//       const token = newToken(user);
 
-      return done(null, { user, token }); // user: {user, token}
-    }
-  )
-);
+//       return done(null, { user, token }); // user: {user, token}
+//     }
+//   )
+// );
 
-module.exports = passport;
+// module.exports = passport;
