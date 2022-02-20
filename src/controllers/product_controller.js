478133fa890c0f2c6be088dae2628d72804b25a6
vamsichamarthi1147 <86275315+chamarthivamsidev@users.noreply.github.com>
2022-02-20 18:43:40 +0530

    updated

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -10,43 +10,20 @@ router.get("/", async (req, res) => {
     return res.status(500).send(err.message);
   }
 });
-// router.get("/api", async (req, res) => {
-//   try {
-//     let products = await Product.find().lean().exec();
-//     // let products = await User.find().lean().exec();
-//     // let products = ["apple", "ball"];
-//     return res.status(200).send(products);
-//   } catch (error) {
-//     return res.status(500).send(err.message);
-//   }
-// });
+
 router.get("/api", async (req, res) => {
   try {
     const page = +req.query.page || 1;
     const size = +req.query.size || 5;
     const skip = (page - 1) * size;
     const products = await Product.find().skip(skip).limit(size).lean().exec();
-    
-    return res.status(201).send( products );
+
+    return res.status(201).send(products);
   } catch (error) {
     return res.status(500).send(err.message);
   }
 });
-// const getAllpro = (model) => async (req, res) => {
-//   // console.log("1")
-//   try {
-//     const page = +req.query.page || 1;
-//     const size = +req.query.size || 5;
-//     const skip = (page - 1) * size;
-//     const items = await model.find().skip(skip).limit(size).lean().exec();
-    
-//     return res.status(201).send( items );
-     
-  
-//   } catch (err) {
-//     return res.status(500).send(err.message);
-//   }
-// };
+
 
 router.post("/products", async (req, res) => {
   try {
@@ -57,8 +34,5 @@ router.post("/products", async (req, res) => {
     return res.status(500).send(err.message);
   }
 });
-
-
-
 
 module.exports = router;
